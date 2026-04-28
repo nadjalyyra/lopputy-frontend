957481993360ae3be29be65758acfe0c817e1989
nadjalyyra <nadjalyyra@gmail.com>
2026-04-28 22:09:47 +0300

    Treenien hakemista varten tehty oma API

---
+++ src/api/trainingapi.ts
@@ -0,0 +1,22 @@
+import type { Training } from "../types";
+
+export const fetchTrainings = () => {
+  return fetch(import.meta.env.VITE_API_URL + "/trainings")
+    .then(res => {
+      if (!res.ok) throw new Error("Error fetching trainings");
+      return res.json();
+    });
+};
+
+export const saveTraining = (training: Training) => {
+  return fetch(import.meta.env.VITE_API_URL + "/trainings", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(training)
+  }).then(res => {
+    if (!res.ok) throw new Error("Error adding training");
+    return res.json();
+  });
+};
