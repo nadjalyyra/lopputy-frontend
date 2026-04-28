2dec7b1caf07d15260cc69deb7829d7e388db2ab
nadjalyyra <nadjalyyra@gmail.com>
2026-04-28 22:36:26 +0300

    Addtrainingin viimeiset hienosäädöt

--- src/components/AddTraining.tsx
+++ src/components/AddTraining.tsx
@@ -3,133 +3,51 @@ import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogTitle from "@mui/material/DialogTitle";
-import MenuItem from "@mui/material/MenuItem";
-import TextField from "@mui/material/TextField";
-import DialogContent from "@mui/material/DialogContent";
+import TrainingForm from "./TrainingForm";
 
 type CustomerOption = {
   value: string;
   label: string;
 };
 
-type Training = {
-  date: string;
-  activity: string;
-  duration: number;
-  customer: string;
-};
-
 type AddTrainingProps = {
-  handleAdd: (training: Training) => void;
+  handleAdd: (training: any) => void;
   customers: CustomerOption[];
 };
 
-export default function AddTraining({
-  handleAdd,
-  customers
-}: AddTrainingProps) {
+export default function AddTraining({ handleAdd, customers }: AddTrainingProps) {
   const [open, setOpen] = useState(false);
 
-  const [training, setTraining] = useState<Training>({
+  const [training, setTraining] = useState({
     date: "",
     activity: "",
     duration: 0,
     customer: ""
   });
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const handleSave = () => {
+  const handleSubmit = () => {
     handleAdd(training);
-
-    setTraining({
-      date: "",
-      activity: "",
-      duration: 0,
-      customer: ""
-    });
-
-    handleClose();
+    setOpen(false);
   };
 
   return (
     <>
-      <Button variant="outlined" onClick={handleClickOpen}>
-        Add Training
+      <Button variant="outlined" onClick={() => setOpen(true)}>
+        LISÄÄ TREENI
       </Button>
 
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>New Training</DialogTitle>
+      <Dialog open={open} onClose={() => setOpen(false)}>
+        <DialogTitle>Uusi treeni</DialogTitle>
 
-        <DialogContent>
-          <TextField
-            label="Activity"
-            value={training.activity}
-            onChange={(e) =>
-              setTraining({ ...training, activity: e.target.value })
-            }
-            fullWidth
-            margin="dense"
-            variant="standard"
-          />
-
-          <TextField
-            label="Duration (min)"
-            type="number"
-            value={training.duration}
-            onChange={(e) =>
-              setTraining({
-                ...training,
-                duration: Number(e.target.value)
-              })
-            }
-            fullWidth
-            margin="dense"
-            variant="standard"
-          />
-
-          <TextField
-            label="Date"
-            type="datetime-local"
-            value={training.date}
-            onChange={(e) =>
-              setTraining({ ...training, date: e.target.value })
-            }
-            fullWidth
-            margin="dense"
-            variant="standard"
-          />
-
-          <TextField
-            select
-            label="Customer"
-            value={training.customer}
-            onChange={(e) =>
-              setTraining({ ...training, customer: e.target.value })
-            }
-            fullWidth
-            margin="dense"
-            variant="standard"
-          >
-            {customers.map((c) => (
-              <MenuItem key={c.value} value={c.value}>
-                {c.label}
-              </MenuItem>
-            ))}
-          </TextField>
-        </DialogContent>
+        <TrainingForm
+          training={training}
+          setTraining={setTraining}
+          customers={customers}
+        />
 
         <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleSave} variant="contained">
-            Save
-          </Button>
+          <Button onClick={() => setOpen(false)}>Peruuta</Button>
+          <Button onClick={handleSubmit}>Tallenna</Button>
         </DialogActions>
       </Dialog>
     </>
